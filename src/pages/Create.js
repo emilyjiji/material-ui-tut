b0c0e1a58ed1b92ad09f7a38ebd7a671ae350a21
emilyjiji <92887308+emilyjiji@users.noreply.github.com>
2023-09-17 11:26:39 -0400

    learned about makeStyles hook

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -4,20 +4,41 @@ import Button from '@material-ui/core/Button';
 import { Container } from '@material-ui/core';
 import SendIcon from '@mui/icons-material/Send';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import { makeStyles } from '@mui/styles';
+
+
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 60,
+    backgroundColor: 'violet',
+    '&:hover': {
+      backgroundColor: 'blue'
+    }
+  },
+  title: {
+    textDecoration: 'underline',
+   
+  }
+})
 
 export default function Create() {
+  const classes = useStyles()
+
   return (
     <Container>
      <Typography
+        className={classes.title}
          variant="h6"
          color="textSecondary"
          component="h2"
+         marginBottom= '20px' 
          gutterBottom
      >
       Create a New Note
      </Typography>
 
      <Button
+     className={classes.btn}
      onClick={() => console.log("You clicked me!")}
       type="submit"
       color="secondary"
