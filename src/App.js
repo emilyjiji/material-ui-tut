efbbc555a0f7a07908f1d76320bb96fce3cda0fb
emilyjiji <92887308+emilyjiji@users.noreply.github.com>
2023-09-17 13:53:03 -0400

    learned about materialui forms and fixed errors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Notes from './pages/Notes'
 import Create from './pages/Create'
+import { ThemeProvider } from '@material-ui/core'
+import { createTheme } from '@material-ui/core/styles' 
+import { purple } from '@mui/material/colors'
+
+const theme = createTheme ({
+  palette: {
+    primary: {
+      main: '#fefefe'
+    }, 
+    secondary: purple
+  },
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightRegular: 600,
+    fontWeightBold: 700,
+    h3: {
+      fontFamily: 'Quicksand',
+    fontWeightLight: 400,
+    }
+  }
+});
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <Router>
       <Switch>
         <Route exact path="/">
@@ -14,6 +38,7 @@ function App() {
         </Route>
       </Switch>
     </Router>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,50 +1,99 @@
 import React from 'react'
+import { useState } from 'react';
 import Typography from '@mui/material/Typography';
 import Button from '@material-ui/core/Button';
-import { Container } from '@material-ui/core';
+import { Container, TextField } from '@material-ui/core';
 import SendIcon from '@mui/icons-material/Send';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import { makeStyles } from '@mui/styles';
 
 
 const useStyles = makeStyles({
-  btn: {
-    fontSize: 60,
-    backgroundColor: 'violet',
-    '&:hover': {
-      backgroundColor: 'blue'
-    }
-  },
-  title: {
-    textDecoration: 'underline',
-   
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: 'block',
+    fontFamily: 'Quicksand',
+    fontWeight: 600,
   }
 })
 
 export default function Create() {
   const classes = useStyles()
 
+  const [title, setTitle] = useState("")
+  const [details, setDetails] = useState("")
+  const [titleError, setTitleError] = useState(false)
+  const [detailsError, setDetailsError] = useState(false)
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setTitleError(false)
+    setDetailsError(false)
+
+    if(title == ''){
+      setTitleError(true)
+    }
+    if(details == ''){
+      setDetailsError(true)
+    }
+    
+    
+    if(title && details) {
+      console.log(title, details)
+    }
+  }
+
   return (
     <Container>
      <Typography
-        className={classes.title}
          variant="h6"
          color="textSecondary"
-         component="h2"
-         marginBottom= '20px' 
+         component="h2" 
          gutterBottom
      >
       Create a New Note
      </Typography>
 
-     <Button
-     className={classes.btn}
+     <form noValidate autoComplete='off' onSubmit={handleSubmit}>
+      <TextField 
+      onChange={(e) => setTitle(e.target.value)}
+      className={classes.field}
+        label="Note Title"
+        variant='outlined'
+        color="secondary"
+        fullWidth
+        required
+        error={titleError}
+      />
+
+<TextField 
+      onChange={(e) => setDetails(e.target.value)}
+      className={classes.field}
+        label="Details"
+        variant='outlined'
+        color="secondary"
+        multiline
+        minRows={4}
+        fullWidth
+        required
+        error={detailsError}
+      />
+
+<Button
      onClick={() => console.log("You clicked me!")}
       type="submit"
       color="secondary"
       variant="contained"
       endIcon={<KeyboardArrowRightIcon />}
      >Submit</Button>
+     </form>
+   
+      
+    
+
+    
 
     {/* icons */}
     {/* <br />
